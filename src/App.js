72ece6b542efda75c4338dadd970e56f3d873523
darrellshi <shi180@purdue.edu>
2018-05-07 15:36:21 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    const init = {
+      method: "GET"
+    }
+    fetch('http://localhost:3000/api/login', init)
+      .then(res => {
+        console.log("We are good to go!")
+      })
+      .catch(err => {
+        console.log(err)
+      })
+      
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
